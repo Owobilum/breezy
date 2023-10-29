ce53bc835b31e3a9974be53fadcf9ef4f9c4fa6b
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2023-10-29 21:24:07 +0100

    convert to PWA

--- src/hooks/useCityWeather.ts
+++ src/hooks/useCityWeather.ts
@@ -4,7 +4,7 @@ import type { IWeatherInfo } from '../types';
 import { getStoredCity, setStoredCityWeather } from '../utils/storage';
 
 const API_KEY = import.meta.env?.VITE_WEATHERSTACK_KEY;
-const BASE_URL = 'http://kapi.weatherstack.com';
+const BASE_URL = 'http://api.weatherstack.com';
 const MAX_RETRIES = 1;
 const RETRY_DELAY = 1000;
 
@@ -44,50 +44,3 @@ function useCityWeather(city: string): IWeatherInfo | null {
 }
 
 export default useCityWeather;
-
-// import { useState, useEffect } from 'react';
-
-// import type { IWeatherInfo } from '../types';
-// import { getStoredCityWeather, setStoredCityWeather } from '../utils/storage';
-
-// const API_KEY = import.meta.env?.VITE_WEATHERSTACK_KEY;
-// const BASE_URL = 'http://kapi.weatherstack.com';
-// const MAX_RETRIES = 1;
-// const RETRY_DELAY = 1000;
-
-// function useCityWeather(city: string): IWeatherInfo | null {
-//   const [cityWeather, setCityWeather] = useState<IWeatherInfo | null>(null);
-//   const [retryCount, setRetryCount] = useState<number>(0);
-
-//   useEffect(() => {
-//     if (!city) return;
-//     async function fetchWeather(query: string): Promise<void> {
-//       try {
-//         const res = await fetch(
-//           `${BASE_URL}/current?access_key=${API_KEY}&query=${query}`
-//         );
-//         const data: IWeatherInfo = await res.json();
-//         setStoredCityWeather(data, city);
-//         setCityWeather(data);
-//       } catch (error) {
-//         // console.error('Error fetching weather data:', error);
-//         if (retryCount < MAX_RETRIES) {
-//           //   console.log(`Retrying in ${RETRY_DELAY / 1000} seconds...`);
-//           setTimeout(() => {
-//             setRetryCount((prevCount) => prevCount + 1);
-//           }, RETRY_DELAY);
-//         } else {
-//           const weather = getStoredCityWeather(city);
-//           setCityWeather(weather);
-//           //   console.error(`Max retries reached. Unable to fetch weather data.`);
-//         }
-//       }
-//     }
-
-//     fetchWeather(city);
-//   }, [city, retryCount]); // Include retryCount in dependencies to trigger retries
-
-//   return cityWeather;
-// }
-
-// export default useCityWeather;
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,28 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+// import appIcon from './src/images/icon.png';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        icons: [
+          {
+            src: '/images/app_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#007bff',
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
